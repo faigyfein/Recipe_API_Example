850bbb978ca4805c37e59c1ea54acb2ab72612b0
faigyfein <faigyfein@gmail.com>
2025-02-09 16:53:26 -0500

    Update recipeAPI.py to hide my API key for security

--- recipeAPI.py
+++ recipeAPI.py
@@ -1,6 +1,6 @@
 import requests
 api_url = "https://api.spoonacular.com/recipes/complexSearch"
-api_key = "7e35b916b5874a56a1fad0bcabfb8c9f"
+api_key = "my_api_key"
 params = { "query": "pizza", "apiKey": api_key}
 response = requests.get(api_url, params=params)
 data = response.json()
